49f5b0719b429505c9c825869cf2ef7a753421d7
Samy0412 <samantha.gadet@gmail.com>
2020-07-28 00:52:13 +0000

    display error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,12 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
+   
   if user.save
     session[:user_id] = user.id
     redirect_to '/'
   else
+    flash[:danger] = user.errors.full_messages.join(", ")
     redirect_to '/signup'
   end
 
